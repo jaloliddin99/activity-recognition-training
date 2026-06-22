bb1a6a507844518b1f554ab222a70eb1923ae2da
jaloliddin99 <jaloliddinabdullaev49@gmail.com>
2026-06-22 14:31:07 +0200

    Switch to 1D CNN, get 87 percent of accuracy after quantization

--- models/config.h
+++ models/config.h
@@ -5,26 +5,26 @@
 #define CONFIG_H
 
 // Model configuration
-#define NUM_CLASSES 11
-#define NUM_FEATURES 6
-#define WINDOW_SIZE 128
+#define NUM_CLASSES 8
+#define NUM_FEATURES 8
+#define WINDOW_SIZE 600
 #define SAMPLING_RATE 50
 
 // Normalization parameters (from training data)
 const float SENSOR_MEAN[NUM_FEATURES] = {
-    0.224560f, 0.060412f, -0.458404f, -0.945975f, 1.756266f, 0.140541f
+    0.108117f, 0.055653f, -0.473147f, -0.949495f, 1.627686f, 0.015654f, 0.993834f, 32.821249f
 };
 
 const float SENSOR_STD[NUM_FEATURES] = {
-    0.580899f, 0.498843f, 0.391174f, 49.164834f, 23.811443f, 20.875010f
+    0.598558f, 0.486906f, 0.409350f, 48.973817f, 24.061215f, 21.929622f, 0.117748f, 48.832386f
 };
 
 // Activity labels
 const char* ACTIVITY_LABELS[NUM_CLASSES] = {
-    "knuckles_cracking", "hand_tapping", "sitting", "standing", "smoking", "ear_rubbing", "hand_scratching", "nail_biting", "hair_pulling", "forehead_rubbing", "nape_rubbing"
+    "knuckles_cracking", "hand_tapping", "sitting", "standing", "smoking", "nail_biting", "hair_pulling", "nape_rubbing"
 };
 
 // Sensor column order (for reference)
-// 0: Ax, 1: Ay, 2: Az, 3: Gx, 4: Gy, 5: Gz
+// 0: Ax, 1: Ay, 2: Az, 3: Gx, 4: Gy, 5: Gz, 6: Acc_mag, 7: Gyro_mag
 
 #endif // CONFIG_H
